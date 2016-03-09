adf2674e99caa8d348517bf9976c945a4fb556d0
Joaquin <hola@jrltt.net>
2016-03-09 23:49:27 +0100

    first approach to zoom element only with toggle a class

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,19 @@
 console.log('\'Allo \'Allo!');
+
+var section = document.querySelector('.section');
+console.log('section');
+section.onclick = function() {
+  console.log('this', this);
+  if (this.dataset.section === 'info') {
+    console.log('fuck yeah');
+    console.log('nav', section.nextSilbing);
+  } else {
+    console.log('nope');
+  }
+}
+
+$(document).ready(function() {
+  $('.click').on('click', function() {
+    $('.estce').toggleClass('zoom');
+  })
+});
