15471366c8550bfac2785262a75b5ae100503556
joaquin reyes <hola@jrltt.net>
2016-05-08 22:56:51 +0200

    fix missing flikrty script

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,6 +2,19 @@
 
 console.log('\'Allo \'Allo!');
 
+var elem = document.querySelector('.gallery');
+/* global Flickity */
+var flkty = new Flickity( elem, {
+  cellAlign: 'left',
+  cellSelector: '.cell',
+  lazyLoad: 1,
+  pageDots: false,
+  imagesLoaded: true,
+  resize: true,
+  percentPosition: false
+});
+// console.log(flkty);
+
 var $;
 $(document).ready(function () {
   $('#menu-toggle').click(function (e) {
